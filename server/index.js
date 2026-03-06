bfe0f74f4fa2d6a03f5412775cd07581f2153be0
googolpl3xianth <68044994+googolpl3xianth@users.noreply.github.com>
2026-03-05 21:36:25 -0800

    Updated UI with svg, Added loading indicator, sync btn, mute btn, copy btn, home btn, userlist + renaming + changing perms, host protocal, anti room spam, updated sync logic, quality selector, removed exposed 8080 port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const io = require('socket.io')(3000, {
     }
 });
 
-const activeRooms = {}; // Memory to store { roomId: { video_name, time, isPaused, users: {username, permisionlv} } }
+const creationSpamFilter = new Map();
+
+const activeRooms = {}; // Memory to store { roomId: { video_name, time, isPaused, users: [{socket.id, username, permision}] } }
 
 const fs = require('fs');
 const path = require('path');
@@ -41,8 +43,18 @@ io.on('connection', (socket) => {
     socket.emit('video-list', list);
 
     socket.on('request-create-room', () => {
+        const userIp = socket.handshake.headers['x-real-ip'] || socket.handshake.address;
+        const now = Date.now();
+
+        if (creationSpamFilter.has(userIp) && (now - creationSpamFilter.get(userIp) < 15000)) {
+            console.log(`Spam blocked from IP: ${userIp}`);
+            return socket.emit('join-error', 'Please wait 15 seconds before creating another room.');
+        }
+
+        creationSpamFilter.set(userIp, now);
+
         const newID = Math.random().toString(36).substring(2, 8);
-        activeRooms[newID] = {currentTime: 0, isPaused: true, users: {}};
+        activeRooms[newID] = {currentTime: 0, isPaused: true, host: null, users: {}};
 
         console.log(`Room created: ${newID} by host: ${socket.id}`);
         socket.emit('room-created', newID);
@@ -53,19 +65,23 @@ io.on('connection', (socket) => {
             socket.join(roomId);
             currentRoomId = roomId;
 
+            let role = 'guest';
             const users = activeRooms[roomId].users;
+
             if(!(socket.id in users)){
-                const hasHost = Object.values(users).includes('host');
-                if(!hasHost){
-                    users[socket.id] = 'host';
+                if(!activeRooms[roomId].host){
+                    role = 'host';
+                    activeRooms[roomId].host = socket.id;
                     console.log(`Roomid: ${currentRoomId} ${socket.id} promoted to Host`);
                 }
-                if (!users[socket.id]) {
-                    users[socket.id] = 'guest';
-                }
+                users[socket.id] = {
+                    role: role,
+                    username:`User-${socket.id.substring(0, 4)}`
+                };
             }
             console.log(`Roomid: ${currentRoomId} User Joined: ${socket.id}`);
-            socket.emit('join-success', users[socket.id]);
+            socket.emit('join-success', users[socket.id].role);
+            io.to(roomId).emit('update-user-list', activeRooms[currentRoomId].users);
         } else {
             socket.emit('join-error', 'This room does not exist or has expired.');
         }
@@ -86,19 +102,30 @@ io.on('connection', (socket) => {
     });
 
     socket.on('request-sync', () => {
+        const room = activeRooms[currentRoomId];
+        
+        if (room && room.host) {
+            if(socket.id !== room.host){
+                socket.to(room.host).emit('get-video-status', { requesterId: socket.id });
+            }
+        }
+    });
+
+    socket.on('host-video-status', (data) => {
         if (activeRooms[currentRoomId]) {
-            console.log(`Requested-sync Roomid: ${currentRoomId} isPaused: ${activeRooms[currentRoomId].isPaused} currentTime: ${activeRooms[currentRoomId].currentTime}`);
-            socket.emit('apply-sync', {
+            activeRooms[currentRoomId].isPaused = data.paused;
+            activeRooms[currentRoomId].currentTime = data.time;
+            socket.to(data.requesterId).emit('apply-sync', {
                 type: activeRooms[currentRoomId].isPaused ? 'pause' : 'play',
                 time: activeRooms[currentRoomId].currentTime
-        });
-    }
+            });
+        }
     });
 
     socket.on('sync-event', (data) => {
         if (!currentRoomId || !activeRooms[currentRoomId]) return;
 
-        const userRole = activeRooms[currentRoomId].users[socket.id];
+        const userRole = activeRooms[currentRoomId].users[socket.id].role;
         if (userRole !== 'host' && userRole !== 'admin') {
             console.log(`Roomid: ${currentRoomId}Permission denied for ${socket.id}`);
             return;
@@ -117,22 +144,86 @@ io.on('connection', (socket) => {
         socket.to(currentRoomId).emit('apply-sync', data);
     });
 
+    socket.on('request-user-change', (targetID, targetUsername, targetRole) => {
+        if (!currentRoomId || !activeRooms[currentRoomId]) return;
+
+        const requesterRole = activeRooms[currentRoomId].users[socket.id].role;
+        const finalID = targetID || socket.id; 
+
+        if (targetRole || (targetID && targetID !== socket.id)) {
+            if (requesterRole !== 'host' && requesterRole !== 'admin') {
+                console.log(`Exploit blocked: Guest ${socket.id} tried to change roles/users.`);
+                return; 
+            } 
+            else if(targetID && targetID !== socket.id && requesterRole === 'admin' && activeRooms[currentRoomId].users[finalID].role === 'host'){
+                console.log(`Exploit blocked: Admin ${socket.id} tried to change roles/users of host.`);
+                return; 
+            }
+        }
+        
+        if (finalID && activeRooms[currentRoomId] && activeRooms[currentRoomId].users[finalID]) {
+            const remainingUserIds = Object.keys(activeRooms[currentRoomId].users);
+            if(targetRole === 'host'){
+                activeRooms[currentRoomId].users[activeRooms[currentRoomId].host].role = 'admin';
+                io.to(currentRoomId).emit('switch-permission', activeRooms[currentRoomId].host, null, 'admin');
+                activeRooms[currentRoomId].host = finalID;
+            }
+            else if(activeRooms[currentRoomId].users[finalID].role === 'host' && (remainingUserIds.length > 1)){
+                let newHostId = remainingUserIds[0];
+                for(const id of remainingUserIds){
+                    if(activeRooms[currentRoomId].users[id].role === 'admin'){
+                        newHostId = id;
+                        break;
+                    }
+                    else if(id != finalID){
+                        newHostId = id;
+                    }
+                }
+                activeRooms[currentRoomId].host = newHostId;
+                activeRooms[currentRoomId].users[newHostId].role = 'host';
+                io.to(currentRoomId).emit('switch-permission', newHostId, null, 'host');
+            }
+            if (targetUsername) {
+                activeRooms[currentRoomId].users[finalID].username = targetUsername;
+            }
+            if (targetRole && (remainingUserIds.length > 1)) {
+                activeRooms[currentRoomId].users[finalID].role = targetRole;
+            }
+            
+            console.log(`Server successfully updated ${finalID} to role: ${targetRole}`);
+            
+            io.to(currentRoomId).emit('switch-permission', finalID, targetUsername, targetRole);
+            io.to(currentRoomId).emit('update-user-list', activeRooms[currentRoomId].users);
+        }
+    });
+
     socket.on('disconnect', () => {
         const roomToCleanup = currentRoomId;
         if (roomToCleanup && activeRooms[roomToCleanup]) {
-            const wasHost = activeRooms[roomToCleanup].users[socket.id] === 'host';
+            const wasHost = activeRooms[roomToCleanup].users[socket.id].role === 'host';
             delete activeRooms[roomToCleanup].users[socket.id];
+            socket.to(roomToCleanup).emit('user-left', socket.id);
 
             if (wasHost) {
+                activeRooms[currentRoomId].host = null;
                 console.log(`Roomid: ${currentRoomId} Host ${socket.id} left. Migrating host...`);
                 
                 const remainingUserIds = Object.keys(activeRooms[currentRoomId].users);
 
                 if (remainingUserIds.length > 0) {
-                    const newHostId = remainingUserIds[0];
-                    activeRooms[currentRoomId].users[newHostId] = 'host';
+                    let newHostId = remainingUserIds[0];
+                    for(const id of remainingUserIds){
+                        if(activeRooms[currentRoomId].users[id].role === 'admin'){
+                            newHostId = id;
+                            break;
+                        }
+                    }
+
+                    activeRooms[currentRoomId].users[newHostId].role = 'host';
+                    activeRooms[currentRoomId].host = newHostId;
                     
-                    io.to(newHostId).emit('switch-permission', 'host');
+                    io.to(currentRoomId).emit('switch-permission', newHostId, null, 'host');
+                    io.to(currentRoomId).emit('update-user-list', activeRooms[currentRoomId].users);
                     console.log(`Roomid: ${currentRoomId} User ${newHostId} promoted to Host.`);
                 }
             }
